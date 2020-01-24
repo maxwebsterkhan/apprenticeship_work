a8d8bf8b48552bd71a80c4ad5b6793766ee5a02e
Max Webster-khan <max.webster-khan@zonedigital.com>
2020-01-24 11:42:53 +0000

    Finished JS slider

--- beginner-javascript-master/exercises/59 - Slider/src/index.js
+++ beginner-javascript-master/exercises/59 - Slider/src/index.js
@@ -2,55 +2,52 @@ function Slider(slider) {
   if (!(slider instanceof Element)) {
     throw new Error('No slider passed in');
   }
-  // create some variables for working iwth the slider
+
+  // create variables for working with the slider.
   let prev;
   let current;
   let next;
-  // select the elements needed for the slider
+
+  // select the elements needed for the slider.
   const slides = slider.querySelector('.slides');
   const prevButton = slider.querySelector('.goToPrev');
   const nextButton = slider.querySelector('.goToNext');
 
   function startSlider() {
     current = slider.querySelector('.current') || slides.firstElementChild;
-    prev = current.previousElementSibling || slides.lastElementChild;
-    next = current.nextElementSibling || slides.firstElementChild;
-    console.log({ current, prev, next });
+    prev = current.previousElementSiblin || slides.lastElementChild;
+    next = current.nextElementSibling;
   }
-
   function applyClasses() {
     current.classList.add('current');
     prev.classList.add('prev');
     next.classList.add('next');
   }
-
   function move(direction) {
-    // first strip all the classes off the current slides
+    // first strip all classes off the current slides
     const classesToRemove = ['prev', 'current', 'next'];
     prev.classList.remove(...classesToRemove);
     current.classList.remove(...classesToRemove);
     next.classList.remove(...classesToRemove);
     if (direction === 'back') {
-      // make an new array of the new values, and destructure them over and into the prev, current and next variables
+      //Make an array of new values and destructure them over into prev, current and next variables
       [prev, current, next] = [
-        // get the prev slide, if there is none, get the last slide from the entire slider for wrapping
+        // get prev slide, if there is none get the slide from the entire slide for wrapping
         prev.previousElementSibling || slides.lastElementChild,
         prev,
-        current,
+        current
       ];
     } else {
       [prev, current, next] = [
         current,
         next,
         // get the next slide, or if it's at the end, loop around and grab the first slide
-        next.nextElementSibling || slides.firstElementChild,
+        next.nextElementSibling || slides.firstElementChild
       ];
     }
-
     applyClasses();
   }
-
-  // when this slider is created, run the start slider function
+  // When slider is created - run start slider function
   startSlider();
   applyClasses();
 
