56bd5b8ea839bc7032eeaf54488009e8b63b3b9a
Max Webster-khan <max.webster-khan@zonedigital.com>
2020-02-18 17:09:18 +0000

    Dad jokes refactored with import/export modules

--- beginner-javascript-master/exercises/77 - Dad Jokes Modules - FINISHED/lib/utils.js
+++ beginner-javascript-master/exercises/77 - Dad Jokes Modules - FINISHED/lib/utils.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 // named export
 export function randomItemFromArray(arr, not) {
-  const item = arr[Math.floor(Math.random() * arr.length)];
-  if (item === not) {
-    console.log('Ahh we used that one last time, look again');
-    return randomItemFromArray(arr, not);
-  }
-  return item;
+        const item = arr[Math.floor(Math.random() * arr.length)];
+        if (item === not) {
+                console.log('Ahh we used that one last time, look again');
+                return randomItemFromArray(arr, not);
+        }
+        return item;
 }
