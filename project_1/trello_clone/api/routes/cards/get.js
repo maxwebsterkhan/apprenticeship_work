33ff28b69f31c219e0d2bafb85129123ef1403f2
Max Webster-khan <max.webster-khan@zonedigital.com>
2020-09-03 10:01:24 +0100

    no message

--- project_1/trello_clone/api/routes/cards/get.js
+++
@@ -1,7 +0,0 @@
-const { cardData } = require("../data");
-
-const getCards = (req, res) => {
- res.send(cardData);
-}
-
-exports.getCards = getCards;
--- project_1/trello_clone/api/routes/data.js
+++
@@ -1,3 +0,0 @@
-const cardData = [];
-
-exports.cardData = cardData;
